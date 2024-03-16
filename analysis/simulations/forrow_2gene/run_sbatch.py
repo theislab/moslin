49d674c872b95b9ac941fb1d8a825ca8dba94687
Zoe Piran <zoepiran@Zoes-MacBook-Pro.local>
2024-03-16 16:38:57 +0200

    2gene

--- analysis/simulations/forrow_2gene/run_sbatch.py
+++ analysis/simulations/forrow_2gene/run_sbatch.py
@@ -18,7 +18,7 @@ for flow_type in [
         "sbatch",
         "--gres=gpu:a5000:1",
         "--time=1-0",
-        "--mem=200gb",
+        "--mem=100gb",
         f"--output={DATA_DIR}/logs/sim-2gene-{flow_type}.log",
         f"--job-name=sim-2gene-{flow_type}",
         "run.sh",
